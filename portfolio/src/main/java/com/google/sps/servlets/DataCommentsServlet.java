8dc9bc00c467b7c3e09d3e4944175beb7b1d4441
Mudit Gupta <muditg317@gmail.com>
2020-06-03 15:38:37 +0000

    add google styleguide formatting for java files

--- portfolio/src/main/java/com/google/sps/servlets/DataCommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataCommentsServlet.java
@@ -14,112 +14,111 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.FetchOptions;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.stream.StreamSupport;
-import java.util.stream.Collectors;
-
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data-comments")
 public class DataCommentsServlet extends HttpServlet {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String urlKey = request.getParameter("key");
-        long id;
-        Key commentEntityKey = null;
-        if (urlKey != null) {
-            id = Long.parseLong(urlKey);
-            commentEntityKey = KeyFactory.createKey("Comment", id);
-        }
-        
-        Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        PreparedQuery results = datastore.prepare(query);
-
-        List<String> comments = new ArrayList<>();
-        boolean foundUrlKeyBasedComment = commentEntityKey == null;
-        String maxCommentParam = request.getParameter("maxComments");
-        int maxComments = maxCommentParam != null ? Integer.parseInt(maxCommentParam) : Integer.MAX_VALUE;
-        for (Entity entity : results.asIterable(FetchOptions.Builder.withLimit(maxComments))) {
-            String comment = (String) entity.getProperty("comment");
-            comments.add(comment);
-            if (!foundUrlKeyBasedComment && entity.getKey().equals(commentEntityKey)) {
-                foundUrlKeyBasedComment = true;
-            }
-            if (comments.size() >= maxComments) {
-                break;
-            }
-        }
-        if (!foundUrlKeyBasedComment) {
-            Entity urlBasedComment;
-            try {
-                urlBasedComment= datastore.get(commentEntityKey);
-                comments.add(0, (String) urlBasedComment.getProperty("comment"));
-            } catch (EntityNotFoundException e) { }
-        }
-        while (comments.size() > maxComments) {
-            comments.remove(maxComments);
-        }
-
-        response.setContentType("application/json;");
-        response.getWriter().println(new Gson().toJson(comments));
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String urlKey = request.getParameter("key");
+    long id;
+    Key commentEntityKey = null;
+    if (urlKey != null) {
+      id = Long.parseLong(urlKey);
+      commentEntityKey = KeyFactory.createKey("Comment", id);
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String comment = request.getParameter("comment");
-        Key key = null;
-        if (comment != null && comment.length() > 0) {
-            Entity commentEntity = new Entity("Comment");
-            commentEntity.setProperty("comment", comment);
-            commentEntity.setProperty("timestamp", System.currentTimeMillis());
-            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-            datastore.put(commentEntity);
-            key = commentEntity.getKey();
-        }
-        response.setContentType("application/json;");
-        // String redirectURL = "/data-comments";
-        if (key != null) {
-            // redirectURL += "?key="+key.getId();
-            response.getWriter().println("{\"key\": " + key.getId() + "}");
-        } else {
-            response.getWriter().println("{}");
-        }
-        // response.sendRedirect(redirectURL);
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    List<String> comments = new ArrayList<>();
+    boolean foundUrlKeyBasedComment = commentEntityKey == null;
+    String maxCommentParam = request.getParameter("maxComments");
+    int maxComments =
+        maxCommentParam != null ? Integer.parseInt(maxCommentParam) : Integer.MAX_VALUE;
+    for (Entity entity : results.asIterable(FetchOptions.Builder.withLimit(maxComments))) {
+      String comment = (String) entity.getProperty("comment");
+      comments.add(comment);
+      if (!foundUrlKeyBasedComment && entity.getKey().equals(commentEntityKey)) {
+        foundUrlKeyBasedComment = true;
+      }
+      if (comments.size() >= maxComments) {
+        break;
+      }
+    }
+    if (!foundUrlKeyBasedComment) {
+      Entity urlBasedComment;
+      try {
+        urlBasedComment = datastore.get(commentEntityKey);
+        comments.add(0, (String) urlBasedComment.getProperty("comment"));
+      } catch (EntityNotFoundException e) {
+      }
+    }
+    while (comments.size() > maxComments) {
+      comments.remove(maxComments);
     }
 
-    @Override
-    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Query query = new Query("Comment");
+    response.setContentType("application/json;");
+    response.getWriter().println(new Gson().toJson(comments));
+  }
 
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        PreparedQuery results = datastore.prepare(query);
-
-        datastore.delete(
-                StreamSupport.stream(results.asIterable().spliterator(), false)
-                .map(Entity::getKey)
-                .collect(Collectors.toList()));
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String comment = request.getParameter("comment");
+    Key key = null;
+    if (comment != null && comment.length() > 0) {
+      Entity commentEntity = new Entity("Comment");
+      commentEntity.setProperty("comment", comment);
+      commentEntity.setProperty("timestamp", System.currentTimeMillis());
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(commentEntity);
+      key = commentEntity.getKey();
     }
+    response.setContentType("application/json;");
+    // String redirectURL = "/data-comments";
+    if (key != null) {
+      // redirectURL += "?key="+key.getId();
+      response.getWriter().println("{\"key\": " + key.getId() + "}");
+    } else {
+      response.getWriter().println("{}");
+    }
+    // response.sendRedirect(redirectURL);
+  }
 
+  @Override
+  public void doDelete(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    Query query = new Query("Comment");
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    datastore.delete(
+        StreamSupport.stream(results.asIterable().spliterator(), false)
+            .map(Entity::getKey)
+            .collect(Collectors.toList()));
+  }
 }
