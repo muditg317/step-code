648d1b000e14cfd18674050fba1ff12b087fdd95
Mudit Gupta <muditg317@gmail.com>
2020-06-04 17:35:07 +0000

    reorganize files and import jinja templating

---
+++ portfolio/src/main/java/com/google/sps/servlets/MainServlet.java
@@ -0,0 +1,58 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.hubspot.jinjava.Jinjava;
+import com.hubspot.jinjava.JinjavaConfig;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns main page content */
+@WebServlet(urlPatterns = {"/", "/main"})
+public class MainServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    System.out.println("REQUEST AT: " + request.getServletPath());
+    if (request.getServletPath() != "" && request.getServletPath() != "/") {
+      response.sendRedirect("/");
+      // return;
+    }
+    response.setContentType("text/html;");
+
+    JinjavaConfig config = new JinjavaConfig();
+    Jinjava jinjava = new Jinjava(config);
+    // jinjava.setResourceLocator(new FileLocator(new File(".")));
+    Map<String, Object> context = new HashMap<>();
+
+    String template =
+        Resources.toString(this.getClass().getResource("/templates/index.html"), Charsets.UTF_8);
+
+    // response.getWriter().println(this.getClass().getResource("/templates/index.html"));
+    // new File(System.getProperty("user.dir") +
+    // "/resources/templates/index.html").toURI().toURL());
+
+    String renderedTemplate = jinjava.render(template, context);
+
+    response.getWriter().println(renderedTemplate);
+  }
+}
