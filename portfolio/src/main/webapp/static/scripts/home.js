75fb9519026f5ee04ea69931a2c66269ca39ee5a
Mudit Gupta <muditg317@gmail.com>
2020-06-08 12:58:48 -0400

    make navbar and title_block generic and customizable components

--- portfolio/src/main/webapp/static/scripts/home.js
+++ portfolio/src/main/webapp/static/scripts/home.js
@@ -1,5 +1,4 @@
 const windowLoadHandler_HOME = () => {
-  setTitleImageWidth();
   setTimeout(() => {
     document.querySelector(".LI-profile-pic").src =
         "https://media-exp1.licdn.com/dms/image/C5603AQGDOaDSpoTQ6g/profile-displayphoto-shrink_200_200/"
